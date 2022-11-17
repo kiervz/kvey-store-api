613763e862311e4ddd3ee55e772a71ca22c14531
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-11-17 14:18:11 +0800

    add cart items model and migration

---
+++ database/migrations/2022_11_17_061505_create_cart_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\User;
+use App\Models\Product;
+
+class CreateCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->foreignIdFor(Product::class, 'product_id');
+            $table->decimal('price', 12, 2);
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
